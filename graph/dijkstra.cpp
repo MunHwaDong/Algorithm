19c6176a8dc6e6fd8bb025264d73f175e3e7f397
MunHwaDong <coming2430@gmail.com>
2024-10-17 15:31:30 +0900

    Update dijkstra.cpp

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -1,89 +1,74 @@
 #include <iostream>
-#include <vector> // 가변 길이 배열
-#include <list> //링크드 리스트 (Doubly Linked List)
-#include <algorithm>
-#include <string>
-#include <map>
-#include <set>
-#include <queue> //우선순위큐 포함
-#include <stack>
-#include <deque>
+#include <vector>
 #include <utility>
-#include <unordered_map>
-#include <unordered_set>
-#include <sstream>
-#include <cmath>
-#include <climits>
-#include <cstring>
-#include <iomanip>
-#include <numeric>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
 
 int main()
 {
-	int n, m; priority_queue<int> q;
+    int n, m;
 
-	cin >> n >> m;
+    cin >> n >> m;
 
-	vector<vector<pair<int, int>>> g(n + 1);
+    vector<vector<pair<int, int>>> g(n + 1);
 
-	for (int i = 0; i < m; ++i)
-	{
-		int v1, v2, w;
+    for(int i = 0; i < m; ++i)
+    {
+        int v1, v2, w;
 
-		cin >> v1 >> v2 >> w;
+        cin >> v1 >> v2 >> w;
 
-		g[v1].push_back({ v2, w });
-	}
+        g[v1].push_back({v2, w});
+    }
 
-	//dijkstra
-	int start = 0;
+    //dijkstra
 
-	cin >> start;
+    int start = 0;
 
-	vector<int> dist(n + 1, int(1e9));
-	vector<bool> isdicision(n + 1, false);
+    cin >> start;
 
-	dist[start] = 0;
-	isdicision[start] = true;
+    vector<int> dist(n + 1, int(1e9));
+    vector<bool> dicision(n + 1, false);
 
-	bool isntfinished = true;
+    dist[start] = 0;
+    dicision[start] = true;
+    bool isntFinished = true;
 
-	//모든 배열이 다 채워질때까지
-	while (isntfinished)
-	{
-		cout << start << endl;
-		for (const auto& ele : g[start])
-		{
-			dist[ele.first] = min(dist[ele.first], ele.second + dist[start]);
-		}
+    while(isntFinished)
+    {
+        for(const auto& ele : g[start])
+        {
+            dist[ele.first] = min(dist[ele.first], dist[start] + ele.second);
+        }
 
-		int minIdx = 0;
+        int minIdx = 0;
 
-		for (int idx = 1; idx <= n; ++idx)
-		{
-			if (isdicision[idx]) continue;
-			if (dist[minIdx] > dist[idx])
-			{
-				minIdx = idx;
-			}
-		}
+        for(int i = 1; i <= n; ++i)
+        {
+            if(dicision[i]) continue;
+            if(dist[minIdx] > dist[i])
+            {
+                minIdx = i;
+            }
+        }
 
-		isdicision[minIdx] = true;
-		start = minIdx;
+        dicision[minIdx] = true;
+        start = minIdx;
 
-		for (int i = 1; i <= n; ++i)
-		{
-			if (isdicision[i] == false) break;
-			else if (i >= n) isntfinished = false;
-		}
-	}
+        for(int i = 1; i <= n; ++i)
+        {
+            if(!dicision[i]) break;
+            else if(i >= n) isntFinished = false;
+        }
 
-	for (const auto& ele : dist)
-	{
-		cout << ele << " ";
-	}
+    }
 
-	return 0;
+    for(const auto& ele : dist)
+    {
+        cout << ele << " ";
+    }
+
+    return 0;
 }
